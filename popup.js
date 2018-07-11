a3eb1737c488e486cdb5c5799fd2088d7a5417de
Cside <cside.story@gmail.com>
2018-07-11 10:47:06 +0900

    bug fix

--- popup.js
+++ popup.js
@@ -1,21 +1,32 @@
 chrome.tabs.getSelected(null, function(selectedTab){
     var currentUrl = selectedTab.url;
-    //var currentUrl = "http://www.hatena.ne.jp/";
     $.get("http://b.hatena.ne.jp/entry/json/" + currentUrl, function(html, status) {
-        var innerHTML = [];
+        const lists = [];
         if (html) {
             if (html.related) {
                 $.each(html.related, function (){
-                    var url        = this.url;
-                    var count      = this.count;
-                    var entryUrl   = this.entry_url;
-                    var title      = truncate(this.title, 40);
-                    var entryHtml  = 
-                        '<li><a target="_blank "href="' + url + '" class="favicon entry-link"><img src="http://favicon.st-hatena.com/?url=' + encodeURIComponent(url) + '" alt=""></a><a target="_blank "href="' + url + '" class="entry-link" title="' + title + '">' + title + '</a><span class="users"><a target="_blank "href="' + entryUrl + '" title="はてなブックマーク - ' + title + '">' + count + ' users</a></span></li>';
+                    const count      = this.count;
+                    const entryUrl   = this.entry_url;
+                    const title      = truncate(this.title, 40);
+                    const url        = this.entry_url.
+                                       replace(/.+entry\/s\//, 'https://').
+                                       replace(/.+entry\//,    'http://');
+                    const entryHtml  = `
+                        <li>
+                            <a target="_blank" href="${url}" class="favicon entry-link">
+                                <img src="http://favicon.st-hatena.com/?url=${encodeURIComponent(url)}" alt="">
+                            </a>
+                            <a target="_blank" href="${url}" class="entry-link">
+                                ${title}
+                            </a>
+                            <span class="users">
+                                <a target="_blank" href="${entryUrl}">${count} users</a>
+                            </span>
+                        </li>`;
 
-                    innerHTML.push(entryHtml);
+                    lists.push(entryHtml);
                 });
-                $('ul.refered').html(innerHTML.join(''));
+                $('ul.refered').html(lists.join(''));
                 return;
             }
         }
@@ -27,8 +38,8 @@ function truncate(str, size, suffix) {
     if (!str) str = '';
     if (!size) size = 32;
     if (!suffix) suffix = '...';
-    var b = 0;
-    for (var i = 0;  i < str.length; i++) {
+    let b = 0;
+    for (let i = 0;  i < str.length; i++) {
         b += str.charCodeAt(i) <= 255 ? 1 : 2;
         if (b > size) {
             return str.substr(0, i) + suffix;
@@ -36,5 +47,3 @@ function truncate(str, size, suffix) {
     }
     return str;
 }
-function a(s){alert(s);}
-function c(s){console.log(s);}
